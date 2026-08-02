a0cb85aa17d64ce798aae5b3856ee65121b51d7d
sabin-vv <vvsabin@gmail.com>
2026-08-02 15:49:28 +0530

    refactor: throw AppError with 404 on task not found

--- backend/src/module/task/service/task.service.ts
+++ backend/src/module/task/service/task.service.ts
@@ -2,6 +2,9 @@ import type { ITaskService } from '../interfaces/task.service.interface'
 import type { ITaskRepository, TaskStatsResult } from '../interfaces/task.repository.interface'
 import type { ITask } from '../types/task.types'
 import { getIO } from '../../../config/socket'
+import { HTTP_STATUS } from '../../../constants/http-status'
+import { TASK_MESSAGES } from '../../../constants/messages'
+import { AppError } from '../../../errors/AppError'
 import mongoose from 'mongoose'
 
 export class TaskService implements ITaskService {
@@ -28,14 +31,14 @@ export class TaskService implements ITaskService {
 
     async update(id: string, userId: string, data: Partial<ITask>): Promise<ITask> {
         const task = await this.repo.update(id, userId, data)
-        if (!task) throw new Error('Task not found')
+        if (!task) throw new AppError(TASK_MESSAGES.NOT_FOUND, HTTP_STATUS.NOT_FOUND)
         getIO().to(userId).emit('task:updated', task)
         return task
     }
 
     async delete(id: string, userId: string): Promise<void> {
         const task = await this.repo.delete(id, userId)
-        if (!task) throw new Error('Task not found')
+        if (!task) throw new AppError(TASK_MESSAGES.NOT_FOUND, HTTP_STATUS.NOT_FOUND)
         getIO().to(userId).emit('task:deleted', task._id)
     }
 
