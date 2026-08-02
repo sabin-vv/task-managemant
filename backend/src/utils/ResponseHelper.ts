a2b36db102d96b3c8e56c3c05ea809212820aab0
sabin-vv <vvsabin@gmail.com>
2026-08-02 15:28:56 +0530

    refactor: standardize response envelope in ResponseHelper

--- backend/src/utils/ResponseHelper.ts
+++ backend/src/utils/ResponseHelper.ts
@@ -1,11 +1,13 @@
 import { type Response } from 'express'
+import { HTTP_STATUS } from '../constants/http-status'
+import { RESPONSE_MESSAGES } from '../constants/messages'
 
 export class ResponseHelper {
-    static success(res: Response, data: unknown, statusCode: number = 200) {
-        res.status(statusCode).json(data)
+    static success(res: Response, data: unknown = null, message: string = RESPONSE_MESSAGES.SUCCESS, statusCode: number = HTTP_STATUS.OK) {
+        res.status(statusCode).json({ success: true, message, data })
     }
 
-    static error(res: Response, message: string, statusCode: number = 400) {
-        res.status(statusCode).json({ message })
+    static error(res: Response, message: string, statusCode: number = HTTP_STATUS.BAD_REQUEST) {
+        res.status(statusCode).json({ success: false, message, data: null })
     }
 }
